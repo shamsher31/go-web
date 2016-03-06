1e0f3db7c0e728f994224e9d1fe93690f32a96bc
Asim <asim@chuhnk.me>
2016-03-06 20:32:09 +0000

    Add hello world

---
+++ examples/helloworld/helloworld.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	log "github.com/golang/glog"
+	"github.com/micro/go-web"
+)
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `<html><body><h1>Hello World</h1></body></html>`)
+}
+
+func main() {
+	service := web.NewService(
+		web.Name("go.micro.web.helloworld"),
+	)
+
+	service.HandleFunc("/", helloWorldHandler)
+
+	if err := service.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
